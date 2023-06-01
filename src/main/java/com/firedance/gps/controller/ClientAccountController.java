e22467ce8797a1ee9e6dc8f096245cbb40d2ab73
qi.tang <qi.tang@infaith.com.cn>
2023-06-01 23:24:34 +0800

    添加dockerfile

--- src/main/java/com/firedance/gps/controller/ClientAccountController.java
+++ src/main/java/com/firedance/gps/controller/ClientAccountController.java
@@ -3,6 +3,8 @@ package com.firedance.gps.controller;
 import com.firedance.gps.handler.result.Result;
 import com.firedance.gps.handler.result.ResultHelper;
 import com.firedance.gps.model.MessageDatagram;
+import com.firedance.gps.model.enums.AccountSpecificationEnum;
+import com.firedance.gps.model.enums.ExceptionEnum;
 import com.firedance.gps.service.IClientAccountService;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,7 +19,10 @@ public class ClientAccountController {
 
     private static List<String> MOUNT_POINTS = new ArrayList<String>();
     static {
-        MOUNT_POINTS.add("");
+        AccountSpecificationEnum[] values = AccountSpecificationEnum.values();
+        for (int i = 0; i < values.length; i++) {
+            MOUNT_POINTS.add(values[i].toString());
+        }
     }
 
     IClientAccountService clientAccountService;
@@ -27,7 +32,10 @@ public class ClientAccountController {
     }
 
     @RequestMapping(value = "/client/login",method = RequestMethod.POST)
-    public Result<Boolean> clientLogin(@RequestParam("account")String account,@RequestParam("pw")String pw){
+    public Result<Boolean> clientLogin(@RequestParam("account")String account,@RequestParam("pw")String pw,@RequestParam("mountPoint")String mountPoint){
+        if(!MOUNT_POINTS.contains(mountPoint)){
+            return ResultHelper.fail(ExceptionEnum.A000001.getCode(),ExceptionEnum.A000001.getMessage(),false );
+        }
         return ResultHelper.success(clientAccountService.login(account,pw));
     }
 
---
+++ src/main/java/com/firedance/gps/model/enums/ExceptionEnum.java
@@ -0,0 +1,28 @@
+package com.firedance.gps.model.enums;
+
+/**
+ * 1天、3天、1个月、2个月、1年、3年
+ * @author qi.tang
+ */
+public enum ExceptionEnum {
+    A000001("A000001","挂载点不在支持列表中"),
+    A000002("A000002","账户或密码错误"),
+    A000003("A000003","账户不可用"),
+    ;
+
+    private String code;
+    private String message;
+
+    ExceptionEnum(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
